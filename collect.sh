266451b643b6e2334de6d2ffb6b22b689d73b655
Li Mi <xjdzch@126.com>
2020-09-19 22:16:13 +0800

    脚本修正

--- collect.sh
+++ collect.sh
@@ -3,5 +3,5 @@ set -x
 mkdir -p release-files
 mv moegirl.dict release-files/
 mv moegirl.dict.yml release-files/
-mv zhwiki.dict release-files/
-mv zhwiki.dict.yaml release-files/
+mv zhwiki-dict/zhwiki.dict release-files/
+mv zhwiki-dict/zhwiki.dict.yaml release-files/
